4b22b8f5e2cd4290ab02a2a29c6084a73afb8ed3
Mohamed-Ghany <73911825+Mohamed-Ghany@users.noreply.github.com>
2020-11-28 11:40:14 +0200

    Object and class tasks 1

---
+++ Object and class tasks 1/CalculateCircleAreaPerimeter.java
@@ -0,0 +1,15 @@
+
+public class CalculateCircleAreaPerimeter {
+	
+public static void main(String[] args) {
+//Create Object from the Circle Class using the constructor :	
+	Circle cir1 = new Circle(1); //the Object..[Class name+new object name = new + ]
+	Circle cir2 = new Circle(25);
+	Circle cir3 = new Circle(125);
+
+//Call the printCircleData method to print the result
+	cir1.printCircleData();
+	cir2.printCircleData();
+	cir3.printCircleData();
+}
+}
